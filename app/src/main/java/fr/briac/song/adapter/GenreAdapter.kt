28a26566b65bcbe69efa290a37bae92dff2630df
bibot <briac.pera04@gmail.com>
2021-09-04 18:27:22 +0200

    test

--- app/src/main/java/fr/briac/song/adapter/GenreAdapter.kt
+++ app/src/main/java/fr/briac/song/adapter/GenreAdapter.kt
@@ -1,6 +1,31 @@
 package fr.briac.song.adapter
 
-class GenreAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import fr.briac.song.R
 
-    class ViewHolder(){}
+class GenreAdapter : RecyclerView.Adapter<GenreAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+
+        val genreImage = view.findViewById<TextView>(R.id.image)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val  view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.genre_template, parent,false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int = 5
 }
--- app/src/main/java/fr/briac/song/fragments/HomeFragment.kt
+++ app/src/main/java/fr/briac/song/fragments/HomeFragment.kt
@@ -5,13 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import fr.briac.song.R
+import fr.briac.song.adapter.GenreAdapter
 
 class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater?.inflate(R.layout.genres, container,false)
+        val view = inflater?.inflate(R.layout.menu_principale, container,false)
+
+        val horizontalRecyclerView = view.findViewById<RecyclerView>(R.id.genres_view)
+        horizontalRecyclerView.adapter = GenreAdapter()
+
+        return  view
     }
 
 }
